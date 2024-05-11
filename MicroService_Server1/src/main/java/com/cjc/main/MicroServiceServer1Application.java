11a1f66fa19d03de31fd69aa809bf7213ddc931c
ravis <ravis@LAPTOP-UPS2M50I>
2024-05-11 16:40:38 +0530

    by ravi

---
+++ MicroService_Server1/src/main/java/com/cjc/main/MicroServiceServer1Application.java
@@ -0,0 +1,15 @@
+package com.cjc.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class MicroServiceServer1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroServiceServer1Application.class, args);
+	}
+
+}
