a9c93264897a830480f4ea7ebb04da9fd2515478
ravis <ravis@LAPTOP-UPS2M50I>
2024-05-11 15:55:36 +0530

    by ravi

--- GithubProject/src/com/cjc/main/A.java
+++ GithubProject/src/com/cjc/main/A.java
@@ -2,12 +2,17 @@ package com.cjc.main;
 
 public class A {
 	
+	public void m1()
+	{
+		System.out.println("M1");
+	}
+	
 	
 
 
 	public static void main(String[] args) {
 		
-System.out.println("A Class");
+		System.out.println("A Class");
 	}
 
 }
